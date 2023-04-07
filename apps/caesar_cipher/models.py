c02357c54a3440787dc9a6aea61a4adf4575f2e5
Natali <natali.dnp.ua@gmail.com>
2023-04-08 00:08:28 +0300

    Add models.py

--- apps/caesar_cipher/models.py
+++ apps/caesar_cipher/models.py
@@ -3,4 +3,5 @@ from django.db import models
 
 class MessageModel(models.Model):
     text = models.TextField(max_length=250)
+    text_crypto = models.TextField(max_length=250)
     key_step = models.IntegerField(default=0, blank=True, null=True)
